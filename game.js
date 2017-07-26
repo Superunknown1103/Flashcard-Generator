0ad019483e23df449ef791cfa7242e5b4589052f
Bruce Tsvil <superunknown1103@gmail.com>
2017-07-26 09:16:20 -0500

    aslsgj

--- game.js
+++ game.js
@@ -7,3 +7,28 @@ for (var i = 0; i < questions.length; i++) {
 	var a = new flashCards.ClozeCard(questions[i].full, questions[i].cloze);
 	closeQuestions.push(a);
 }
+
+var currentQuestion = 0;
+var answerRight = 0;
+var anwserWrong = 0;
+
+function askQuestion() {
+	inquirer.prompt([
+	{
+		type: 'input',
+		message: closeQuestions[currentQuestion].partial + '\nAnswer: ',
+		name: 'userGuess'
+	}
+
+	]).then(function (answers) {
+		console.log('\n');
+
+		if (answers.userGuess.toLowerCase() === closeQuestions[currentQuestion].cloze.toLowerCase()) {
+			console.log('Correct!');
+			answerRight++;
+		} else {
+			console.log('Incorrect!');
+			answerWrong++;
+		}
+	})
+}
